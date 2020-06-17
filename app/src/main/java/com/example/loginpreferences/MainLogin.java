91521162d8ab18fecf171794d87544562510c6a4
muksin99 <muksin654@gmail.com>
2020-06-17 18:57:24 +0700

    LoginPreferences

---
+++ app/src/main/java/com/example/loginpreferences/MainLogin.java
@@ -0,0 +1,14 @@
+package com.example.loginpreferences;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
---
+++ app/src/main/java/com/example/loginpreferences/MainRegister.java
@@ -0,0 +1,13 @@
+package com.example.loginpreferences;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
